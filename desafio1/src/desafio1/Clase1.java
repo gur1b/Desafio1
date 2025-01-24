d2a50d1403bfa9753b3b45df4e7afa4656c53474
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-01-24 01:09:22 -0300

    Desafio 1

---
+++ desafio1/src/desafio1/Clase1.java
@@ -0,0 +1,21 @@
+package desafio1;
+
+public class Clase1 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		int valorEntero = 10;
+		
+		float valorDecimal = 10.3f; 
+	
+		System.out.println("=============Desafío 1===============");
+		
+
+		System.out.println("Valor Entero: "  + valorEntero);
+		System.out.println("Valor Decimal: "  + valorDecimal);
+		
+	}
+
+}
